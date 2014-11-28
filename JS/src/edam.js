339a5b4f8a1ccda721e41bce98f83ec3e02ea9cc
mautematico <mauricio@navarromiranda.mx>
2014-11-28 07:31:17 -0600

    Primera versión que se hace algo (y se nota). :)

--- JS/src/edam.js
+++ JS/src/edam.js
@@ -1,30 +1,41 @@
 i = 0;
+var a = [];
+var imagen = document.createElement("image");
+var span = document.createElement("span");
+
+
+function limpiar(){
+	var miNode = document.getElementById('markov');
+	while (miNode.firstChild) {
+    miNode.removeChild(miNode.firstChild);
+	}
+	
+	var myNode = document.getElementById("imagen");
+	while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+	}
+	
+	
+}
 
 
 function adem(){
-	for(j=0;j<100;j++){
-		i = markovMasMenosUno(i);
-		console.log(i);
-
-	}
-}
-
-/*function runForEver(func, delay) {
-    func();
-    setTimeout(function() { runForEver(func, delay); }, delay);
-}
-
-
-runForEver(adem, 1000);
-*/
-setInterval(function(){ 
-adem();	
-}, 500);
-
-
-/*
-var a = [];
-	for(j = 0; j < 25600; j++){
+	limpiar();
+	i = markovMasMenosUno(i);
+	console.log(i);
+	span = document.createElement("span");
+	span.textContent = i;
+	document.getElementById('markov').appendChild(span);
+				
+	for(j = 0; j < 8192; j++){
 		a[j] = Math.floor(Math.random()*256);
+	}
+	//	console.log(a);	
+	imagen = drawArray(a, 32);
+	document.getElementById('imagen').appendChild(imagen);
+	
 }
-console.log(a);*/
+
+setInterval(function(){
+	adem();
+}, 500);
